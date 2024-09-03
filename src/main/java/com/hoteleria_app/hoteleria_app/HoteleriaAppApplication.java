b502162c17e5a629982210ac98efa27ef20f5eb0
Juan Chuc <dev.juan.eloper@gmail.com>
2024-09-02 22:24:55 -0600

    Initialize project with spring boot

---
+++ src/main/java/com/hoteleria_app/hoteleria_app/HoteleriaAppApplication.java
@@ -0,0 +1,13 @@
+package com.hoteleria_app.hoteleria_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteleriaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteleriaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoteleria_app/hoteleria_app/HoteleriaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoteleria_app.hoteleria_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteleriaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
